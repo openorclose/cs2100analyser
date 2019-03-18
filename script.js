2c4dd0a91653d33033f3bbdbf5da8fc04d5ce364
openorclose <interfiancee@gmail.com>
2019-03-18 23:31:08 +0800

    Added rows to tbody instead of table

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const labTable = document.getElementById("lab-table");
 const tableHead = labTable.createTHead().insertRow();
+const tableBody = labTable.createTBody();
 for (const columnTitle of data.lab.header) {
     const td = tableHead.insertCell();
     td.appendChild(document.createTextNode(columnTitle));
 }
 for (const row of data.lab.rows) {
-    const tr = labTable.insertRow();
+    const tr = tableBody.insertRow();
     for (const col of row) {
         const td = tr.insertCell();
         td.appendChild(document.createTextNode(col));
